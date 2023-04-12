3d5208bae237ce80beaac2ca8fe812d184022c8e
xiaotaatee <zy15161@163.com>
2023-04-12 17:48:00 +0800

    cto first upload

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img src="@/assets/logo.png">
+  <div id="">
+    <div id="">账号：<input type="text" v-model="account"></div>
+    <div id="">密码：<input type="password" name="" id="" v-model="passwprd"></div>
+    <input type="button" value="登录" @click="login">
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+   return ({
+      account:"",
+    password:""
+   })
+  },
+  methods:{
+    login(){
+      if(!this.account){
+        alert("用户名不能为空，请输入用户名")
+      }
+       if(!this.password){
+         alert("密码不能为空，请输入密码")
+       }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue' //create不是构造函数而是工厂函数
 import App from './App.vue'
 
 createApp(App).mount('#app')
